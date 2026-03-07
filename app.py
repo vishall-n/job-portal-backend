3e91173ff275c7161c1d9df706d4dfb18c07baff
vishal <23btrcm026@jainuniversity.ac.in>
2026-03-07 16:17:08 +0530

    fix host and port for render

--- app.py
+++ app.py
@@ -244,4 +244,6 @@ def update_job(job_id):
 
 # ---- THIS MUST ALWAYS BE AT THE VERY END ----
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT",5000))
+    app.run(host="0.0.0.0", port=port)
--- db.py
+++ db.py
@@ -1,10 +1,10 @@
 import mysql.connector
 
 db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="1234",   
-    database="job_portal"
+    host="sql12.freesqldatabase.com",
+    user="sql12819216",
+    password="43XhGZS8wA",
+    database="sql12819216"
 )
 
 cursor = db.cursor()
